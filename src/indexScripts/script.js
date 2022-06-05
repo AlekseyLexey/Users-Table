829d3d3828375c2c1f2cde1631ff32f8dfdbfb6c
Aleksey <axcerz@gmail.com>
2022-06-06 01:28:24 +1000

    Функционал с дефолтной бд готов

--- src/indexScripts/script.js
+++ src/indexScripts/script.js
@@ -44,11 +44,34 @@ pagination.on('move', nextPage => {
 	navigator.set('page', nextPage);
 });
 
+const userNameInput = document.querySelector('[data-field="userName"]');
 
-// const usersAges = findUsersByAge(25);
+userNameInput.addEventListener('input', async e => {
+  const target = e.target;
+  const userNames = await findUsersByName(target.value);
 
-// usersAges.then(users => users.forEach(item => {
-//   console.log(item);
-// }));
+  if (userNames.length) {
+    ut.users = userNames;
+    pagination.pages = 1;
+  } else {
+    ut.users = JSON.parse(localStorage.getItem('__users__')).slice(0, 100);
+    pagination.pages = Math.ceil(JSON.parse(localStorage.getItem('__users__')).length / 100);
+    pagination.page = 1;
+  }
+});
 
-// getAllUsersCounts().then(count => console.log(count));
+const userAgesInput = document.querySelector('[data-field="userAge"]');
+
+userAgesInput.addEventListener('input', async e => {
+  const target = e.target;
+  const userAges = await findUsersByAge(+target.value);
+
+  if (userAges.length) {
+    ut.users = userAges;
+    pagination.pages = 1;
+  } else {
+    ut.users = JSON.parse(localStorage.getItem('__users__')).slice(0, 100);
+    pagination.pages = Math.ceil(JSON.parse(localStorage.getItem('__users__')).length / 100);
+    pagination.page = 1;
+  }
+});
